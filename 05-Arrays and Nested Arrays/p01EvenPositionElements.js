e7bc44bae146316b7abbd1b9ccc4463984994f4b
iliyazz <iliyaz.softuni@gmail.com>
2022-09-17 10:29:49 +0300

    Create p01EvenPositionElements.js

---
+++ 05-Arrays and Nested Arrays/p01EvenPositionElements.js
@@ -0,0 +1,12 @@
+function EvenPositionElements(arr){
+    let result = [];
+    for(let i = 0; i < arr.length; i++){
+        if(i % 2 === 0){
+            result.push(arr[i]);
+        }
+    }
+    console.log(result.join(' '))
+}
+EvenPositionElements(['20', '30', '40', '50', '60']);
+EvenPositionElements('---------------------------');
+EvenPositionElements(['5', '10']);
