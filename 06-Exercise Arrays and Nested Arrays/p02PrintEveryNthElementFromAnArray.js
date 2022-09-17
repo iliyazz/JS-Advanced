4088491308588e3202ca23292420b04f098df6b8
iliyazz <iliyaz.softuni@gmail.com>
2022-09-17 18:22:48 +0300

    Create p02PrintEveryNthElementFromAnArray.js

---
+++ 06-Exercise Arrays and Nested Arrays/p02PrintEveryNthElementFromAnArray.js
@@ -0,0 +1,31 @@
+function printEveryNthElementFromAnArray(arr, step){
+    let result = [];
+    for(let i = 0; i < arr.length; i += step){
+        result.push(arr[i]);
+    }
+    //console.log(result);
+    return result;
+}
+
+printEveryNthElementFromAnArray(
+    ['5', 
+'20', 
+'31', 
+'4', 
+'20'], 
+2);
+console.log('----------------------')
+printEveryNthElementFromAnArray(
+    ['dsa',
+'asd', 
+'test', 
+'tset'], 
+2);
+console.log('----------------------')
+printEveryNthElementFromAnArray(
+    ['1', 
+'2',
+'3', 
+'4', 
+'5'], 
+6);
