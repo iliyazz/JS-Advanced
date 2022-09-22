769a51a14c6e9f41597f6b7883b7601220971fc6
iliyazz <iliyaz.softuni@gmail.com>
2022-09-23 00:00:03 +0300

    Create p03CarFactoryRefactoring .js

---
+++ 08-Exercise Objects and Composition/p03CarFactoryRefactoring .js
@@ -0,0 +1,44 @@
+function carFactory(car) {
+    const engineEnum = {
+        "Small engine": { power: 90, volume: 1800 },
+        "Normal engine": { power: 120, volume: 2400 },
+        "Monster engine": { power: 200, volume: 3500 }
+    }
+    resultCar = {};
+    resultCar["model"] = car.model;
+    resultCar["engine"] = {};
+    if(car.power <= 90){
+        resultCar.engine.power = engineEnum["Small engine"].power;
+        resultCar.engine.volume = engineEnum["Small engine"].volume;
+    }
+    else if(car.power <= 120){
+        resultCar.engine.power = engineEnum["Normal engine"].power;
+        resultCar.engine.volume = engineEnum["Normal engine"].volume;
+    }
+    else{
+        resultCar.engine.power = engineEnum["Monster engine"].power;
+        resultCar.engine.volume = engineEnum["Monster engine"].volume;
+    }
+    resultCar.carriage = {type: car.carriage, color: car.color}
+    let size = car.wheelsize % 2 === 0 ? car.wheelsize - 1 : car.wheelsize;
+    resultCar["wheels"] = new Array(4).fill(size);
+    console.table(resultCar);
+    console.log(resultCar);
+    return resultCar;
+}
+
+carFactory({
+    model: 'VW Golf II',
+    power: 90,
+    color: 'blue',
+    carriage: 'hatchback',
+    wheelsize: 14
+});
+console.log('---------------')
+carFactory({
+    model: 'Opel Vectra',
+    power: 110,
+    color: 'grey',
+    carriage: 'coupe',
+    wheelsize: 17
+});
