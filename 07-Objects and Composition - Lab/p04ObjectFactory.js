37b55669e91217627b80406f6e1be5443f2af1bb
iliyazz <iliyaz.softuni@gmail.com>
2022-09-22 10:51:30 +0300

    Create p04ObjectFactory.js

---
+++ 07-Objects and Composition - Lab/p04ObjectFactory.js
@@ -0,0 +1,46 @@
+function objectFactory(library, orders){
+    let result = [];
+    for(let order of orders){
+        const current = Object.assign({}, order.template);
+        for(let part of order.parts){
+            let func = library[part]
+            current[part] = func;
+        }
+        result.push(current);
+    }
+    return result;
+}
+
+
+const library = {
+    print: function () {
+      console.log(`${this.name} is printing a page`);
+    },
+    scan: function () {
+      console.log(`${this.name} is scanning a document`);
+    },
+    play: function (artist, track) {
+      console.log(`${this.name} is playing '${track}' by ${artist}`);
+    },
+  };
+  const orders = [
+    {
+      template: { name: 'ACME Printer'},
+      parts: ['print']
+    },
+    {
+      template: { name: 'Initech Scanner'},
+      parts: ['scan']
+    },
+    {
+      template: { name: 'ComTron Copier'},
+      parts: ['scan', 'print']
+    },
+    {
+      template: { name: 'BoomBox Stereo'},
+      parts: ['play']
+    }
+  ];
+  
+  const products = objectFactory(library, orders);
+  console.table(products);
