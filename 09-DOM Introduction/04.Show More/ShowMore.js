bddf873c858babb73ae5fd67892c7347b90c90e7
iliyazz <iliyaz.softuni@gmail.com>
2022-09-24 18:26:16 +0300

    Create Show More

---
+++ 09-DOM Introduction/04.Show More/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    let shortText = document.getElementById('more');
+    let fullText = document.getElementById('text');
+    shortText.style.display = 'none';
+    fullText.style.display = 'inline';
+}
