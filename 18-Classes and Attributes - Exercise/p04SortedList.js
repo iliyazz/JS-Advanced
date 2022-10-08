743ee4ae7dfad28852c2b60ba919dda09e0c754c
iliyazz <iliyaz.softuni@gmail.com>
2022-10-08 17:10:30 +0300

    Create p04SortedList.js

---
+++ 18-Classes and Attributes - Exercise/p04SortedList.js
@@ -0,0 +1,38 @@
+class List {
+    constructor() {
+        this.arr = [];
+    }
+    add(element) {
+        this.arr.push(Number(element));
+        this.arr = this.arr.sort((a, b) => a - b);
+        this.size++;
+        debugger
+    }
+    remove(index) {
+        if (index >= 0 && index < this.size) {
+            this.arr.splice(index, 1);
+            this.size--;
+            debugger
+        }
+        else{
+            throw new Error('index is out of range');
+        }
+    }
+    get(index) {
+        debugger
+        if (index >= 0 && index < this.size) {
+            return this.arr[index];
+        }
+        throw new Error('index is out of range');
+    }
+    size = 0;
+}
+let list = new List();
+debugger
+list.add(5);
+list.add(6);
+list.add(7);
+console.log(list.get(3));
+list.remove(1);
+console.log(list.get(1));
+console.log(list.arr.join(', '))
